20348d787e4015c2b3e7f4cf4ff23bb2a6e4ad70
Moser, Kevin <Kevin.Moser@nordstrom.com>
2015-12-22 11:07:46 -0800

    Use a recursive call for timeout

--- apply-labels.sh
+++ apply-labels.sh
@@ -1,51 +1,50 @@
 #!/bin/sh
 
-timeout 10s /bin/sh <<EOS
-#!/bin/sh
+if [ "$1" == "" ]; then
+  timeout 300s $0 notimeout
+else
+  MD="curl -s http://169.254.169.254/latest/meta-data/"
+  AWS_REGION=`$MD/placement/availability-zone | head -c -1`
+  AVAILABILITY_ZONE=`$MD/placement/availability-zone`
+  INSTANCE_ID=`${MD}/instance-id`
+  INSTANCE_TYPE=`${MD}/instance-type`
+  SECURITY_GROUPS=`${MD}/security-groups | tr '\n' ','`
 
-MD="curl -s http://169.254.169.254/latest/meta-data/"
-AWS_REGION=`$MD/placement/availability-zone | head -c -1`
-AVAILABILITY_ZONE=`$MD/placement/availability-zone`
-INSTANCE_ID=`${MD}/instance-id`
-INSTANCE_TYPE=`${MD}/instance-type`
-SECURITY_GROUPS=`${MD}/security-groups | tr '\n' ','`
+  JQ_VERSION=`jq --version`
+  echo "[$(date)] jq-version: $JQ_VERSION"
 
-JQ_VERSION=`jq --version`
-echo "[$(date)] jq-version: $JQ_VERSION"
+  # It appears it takes a while for the hostname to incorporate the node name.
+  while [ "x$NODE" = "x" ] || [ "$NODE" = "null" ]; do
+    HOSTNAME=`hostname`
+    echo "[$(date)] Hostname: $HOSTNAME"
+    NODE=`curl  -s -f \
+          --cert   /etc/kubernetes/ssl/worker.pem \
+          --key    /etc/kubernetes/ssl/worker-key.pem \
+          --cacert /etc/kubernetes/ssl/ca.pem  \
+          https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/kube-system/pods/${HOSTNAME} | jq -r '.spec.nodeName'
+    `
+    echo "[$(date)] Node: $NODE"
 
+    sleep 1
+  done
 
-# It appears it takes a while for the hostname to incorporate the node name.
-while [ "x$NODE" = "x" ] || [ "$NODE" = "null" ]; do
-  HOSTNAME=`hostname`
-  echo "[$(date)] Hostname: $HOSTNAME"
-  NODE=`curl  -s -f \
+  echo "[$(date)] Node: $NODE"
+
+  INSTANCE_DETAILS=`aws --region "$AWS_REGION" ec2 describe-instances --instance-id "$INSTANCE_ID"`
+
+  SUBNET_ID=`echo $INSTANCE_DETAILS | jq -r '.Reservations[].Instances[].NetworkInterfaces[].SubnetId'`
+  INSTANCE_PROFILE_ARN=`echo $INSTANCE_DETAILS | jq -r '.Reservations[].Instances[].IamInstanceProfile.Arn'`
+  INSTANCE_PROFILE_ID=`echo $INSTANCE_DETAILS | jq -r '.Reservations[].Instances[].IamInstanceProfile.Id'`
+  TAGS_LABELS=`echo $INSTANCE_DETAILS | jq -r '.Reservations[].Instances[].Tags | map("\"aws.amazon.com/tags-\(.Key)\"%\"\(.Value)\"") | join(",\n")' | tr ":" "-" | tr "%" ":"`
+
+  curl  -s \
         --cert   /etc/kubernetes/ssl/worker.pem \
         --key    /etc/kubernetes/ssl/worker-key.pem \
         --cacert /etc/kubernetes/ssl/ca.pem  \
-        https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/kube-system/pods/${HOSTNAME} | jq -r '.spec.nodeName'
-  `
-  echo "[$(date)] Node: $NODE"
-
-  sleep 1
-done
-
-echo "[$(date)] Node: $NODE"
-
-INSTANCE_DETAILS=`aws --region "$AWS_REGION" ec2 describe-instances --instance-id "$INSTANCE_ID"`
-
-SUBNET_ID=`echo $INSTANCE_DETAILS | jq -r '.Reservations[].Instances[].NetworkInterfaces[].SubnetId'`
-INSTANCE_PROFILE_ARN=`echo $INSTANCE_DETAILS | jq -r '.Reservations[].Instances[].IamInstanceProfile.Arn'`
-INSTANCE_PROFILE_ID=`echo $INSTANCE_DETAILS | jq -r '.Reservations[].Instances[].IamInstanceProfile.Id'`
-TAGS_LABELS=`echo $INSTANCE_DETAILS | jq -r '.Reservations[].Instances[].Tags | map("\"aws.amazon.com/tags-\(.Key)\"%\"\(.Value)\"") | join(",\n")' | tr ":" "-" | tr "%" ":"`
-
-curl  -s \
-      --cert   /etc/kubernetes/ssl/worker.pem \
-      --key    /etc/kubernetes/ssl/worker-key.pem \
-      --cacert /etc/kubernetes/ssl/ca.pem  \
-      --request PATCH \
-      -H "Content-Type: application/strategic-merge-patch+json" \
-      -d @- \
-      https://${KUBERNETES_SERVICE_HOST}/api/v1/nodes/${NODE} <<EOF
+        --request PATCH \
+        -H "Content-Type: application/strategic-merge-patch+json" \
+        -d @- \
+        https://${KUBERNETES_SERVICE_HOST}/api/v1/nodes/${NODE} <<EOF
 {
   "metadata": {
     "labels": {
@@ -61,4 +60,4 @@ curl  -s \
   } 
 }
 EOF
-EOS
+fi
