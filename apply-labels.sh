fefb4382cbd08f6a07aca4310db8cbcfaa9209d5
Moser, Kevin <Kevin.Moser@nordstrom.com>
2016-03-28 15:27:45 -0700

    Remove unused labels, and labels supplied by v1.2

--- apply-labels.sh
+++ apply-labels.sh
@@ -13,11 +13,7 @@ else
   echo "[$(date)] $0 started without timeout"
 
   MD="curl -s http://169.254.169.254/latest/meta-data/"
-  AWS_REGION=`$MD/placement/availability-zone | head -c -1`
-  AVAILABILITY_ZONE=`$MD/placement/availability-zone`
   INSTANCE_ID=`${MD}/instance-id`
-  INSTANCE_TYPE=`${MD}/instance-type`
-  SECURITY_GROUPS=`${MD}/security-groups | tr '\n' ','`
 
   JQ_VERSION=`jq --version`
   echo "[$(date)] jq-version: $JQ_VERSION"
@@ -42,9 +38,6 @@ else
   INSTANCE_DETAILS=`aws --region "$AWS_REGION" ec2 describe-instances --instance-id "$INSTANCE_ID"`
 
   SUBNET_ID=`echo $INSTANCE_DETAILS | jq -r '.Reservations[].Instances[].NetworkInterfaces[].SubnetId'`
-  INSTANCE_PROFILE_ARN=`echo $INSTANCE_DETAILS | jq -r '.Reservations[].Instances[].IamInstanceProfile.Arn'`
-  INSTANCE_PROFILE_ID=`echo $INSTANCE_DETAILS | jq -r '.Reservations[].Instances[].IamInstanceProfile.Id'`
-  TAGS_LABELS=`echo $INSTANCE_DETAILS | jq -r '.Reservations[].Instances[].Tags | map("\"aws.amazon.com/tags-\(.Key)\"%\"\(.Value)\"") | join(",\n")' | tr ":" "-" | tr "%" ":"`
 
   curl  -s \
         --cert   /etc/kubernetes/ssl/worker.pem \
@@ -57,15 +50,9 @@ else
 {
   "metadata": {
     "labels": {
-      "aws.amazon.com/region":               "${AWS_REGION}",
-      "aws.amazon.com/az":                   "${AVAILABILITY_ZONE}",
-      "aws.amazon.com/instance-id":          "${INSTANCE_ID}",
-      "aws.amazon.com/instance-type":        "${INSTANCE_TYPE}",
-      "aws.amazon.com/subnet-id":            "${SUBNET_ID}"
-    },
-    "annotations": {
-      "aws.node.kubernetes.io/sgs":  "${SECURITY_GROUPS}"
-    } 
+      "aws.amazon.com/instance-id": "${INSTANCE_ID}",
+      "aws.amazon.com/subnet-id": "${SUBNET_ID}"
+    }
   } 
 }
 EOF
